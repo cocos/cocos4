e9b7120939247d7d2fc403cd236330823557a24c
tangkai <1944876319@qq.com>
2025-12-20 14:46:00 +0800

    add interface of notifyMiniProgramPlayableStatus, setEnableDebug

--- @types/pal/minigame.d.ts
+++ @types/pal/minigame.d.ts
@@ -79,6 +79,8 @@ declare module 'pal/minigame' {
         offFeedStatusChange(cb?: (type: string) => void): void;
         storeFeedData(opt: storeFeedDataOptions): void;
         getFeedData(opt: getFeedDataOptions): void;
+        setEnableDebug?(parm: DebugParameter): void;
+        notifyMiniProgramPlayableStatus?(parm: notiOptions): void;
     }
 
     interface WeChatAPI {
@@ -429,5 +431,18 @@ interface getFeedDataOptions {
     contentID: string;
     success?: (successObj: {errMsg: string, extra: string, status: number}) => void,
     fail?: (failObj: commonFailObj) => void,
+    complete?: () => void,
+}
+
+export interface DebugParameter {
+    setEnableDebug: boolean,
+    success?: () => void,
+    fail?: (err: any) => void,
+    complete?: () => void,
+}
+
+export interface notiOptions {
+    success?: () => void,
+    fail?: (err: any) => void,
     complete?: () => void,
 }
