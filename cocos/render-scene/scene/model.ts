d0aafb9bb815abb4ac79f651f9f0ebdd41f94b00
bofeng-song <song2008_2001@126.com>
2026-02-03 17:26:06 +0800

    Fix the issue that the implementation of Vec3 equals is inconsistent with the implementations of Vec2 and Vec4. (#57)
    * Fix the issue that the implementation of Vec3 equals is inconsistent with the implementations of Vec2 and Vec4.
    
    * Change approxEquals method to return boolean
    
    * Fix formatting of approxEquals method
    
    ---------
    
    Co-authored-by: hyde zhou <zlzhou.sh@outlook.com>

--- cocos/render-scene/scene/model.ts
+++ cocos/render-scene/scene/model.ts
@@ -824,7 +824,7 @@ export class Model {
         }
 
         const center = this._worldBounds!.center;
-        if (!EDITOR && center.equals(this._lastWorldBoundCenter, EPSILON)) {
+        if (!EDITOR && center.approxEquals(this._lastWorldBoundCenter, EPSILON)) {
             return;
         }
 
