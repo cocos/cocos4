f79975e73b1fe0718704ca3855674fd181454d5b
Zhou Zhenglong <zlzhou.sh@outlook.com>
2026-04-28 17:04:02 +0800

    refactor platforms

--- cocos/asset/assets/image-asset.jsb.ts
+++ cocos/asset/assets/image-asset.jsb.ts
@@ -22,7 +22,7 @@
  THE SOFTWARE.
 */
 
-import { ALIPAY, XIAOMI, JSB, TEST, EDITOR, NODEJS } from 'internal:constants';
+import { ALIPAY, JSB, TEST, EDITOR, NODEJS } from 'internal:constants';
 import { Format, FormatFeatureBit, deviceManager } from '../../gfx';
 import { PixelFormat } from './asset-enum';
 import { sys, macro, warnID, cclegacy } from '../../core';
@@ -54,7 +54,7 @@ function isImageBitmap (imageSource: any): boolean {
 }
 
 function isNativeImage (imageSource: ImageSource): imageSource is (HTMLImageElement | HTMLCanvasElement | ImageBitmap) {
-    if (ALIPAY || XIAOMI) {
+    if (ALIPAY) {
         // We're unable to grab the constructors of Alipay native image or canvas object.
         return !('_data' in imageSource);
     }
