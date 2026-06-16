61362a14c29d1f0d2e579c5075bc47d843a25c84
tangkai <1944876319@qq.com>
2026-06-16 16:26:04 +0800

    change sudv2 to openpaas

--- pal/system-info/minigame/system-info.ts
+++ pal/system-info/minigame/system-info.ts
@@ -23,7 +23,7 @@
  THE SOFTWARE.
 */
 
-import { ALIPAY, BYTEDANCE, HUAWEI, OPPO, RUNTIME_BASED, SUD, SUDV2, VIVO, MIGU, HONOR, WECHAT, XIAOMI, DEBUG, TEST, TAOBAO, TAOBAO_MINIGAME, WECHAT_MINI_PROGRAM } from 'internal:constants';
+import { ALIPAY, BYTEDANCE, HUAWEI, OPPO, RUNTIME_BASED, SUD, OpenPAAS, VIVO, MIGU, HONOR, WECHAT, XIAOMI, DEBUG, TEST, TAOBAO, TAOBAO_MINIGAME, WECHAT_MINI_PROGRAM } from 'internal:constants';
 import { minigame, SystemInfo as MinigameSystemInfo, WindowInfo } from 'pal/minigame';
 import { IFeatureMap } from 'pal/system-info';
 import { EventTarget } from '../../../cocos/core/event';
@@ -60,8 +60,8 @@ if (WECHAT) {
     currentPlatform = Platform.HONOR_MINI_GAME;
 } else if (SUD) {
     currentPlatform = Platform.SUD_MINI_GAME;
-} else if (SUDV2) {
-    currentPlatform = Platform.SUDV2_MINI_GAME;
+} else if (OpenPAAS) {
+    currentPlatform = Platform.OpenPAAS;
 }
 
 let isVersionGreaterOrEqualTo;
