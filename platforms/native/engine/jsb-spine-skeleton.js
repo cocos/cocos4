691e49168688a7c36c2b9d7c876ce7cc5a5e7ff1
zxx43 <zxx43@hotmail.com>
2026-07-21 11:52:06 +0800

    use warn to replace console.log

--- platforms/native/engine/jsb-spine-skeleton.js
+++ platforms/native/engine/jsb-spine-skeleton.js
@@ -160,7 +160,7 @@ const cacheManager = require('./jsb-cache-manager');
                 const loopCount = Math.floor(trackEntry.trackTime / duration);
                 this._compeleteListener && this._compeleteListener(trackEntry, loopCount);
             } else {
-                console.log("Start time must bigger than end time!");
+                cc.warn("Start time must bigger than end time!");
             }
         });
     };
@@ -174,7 +174,7 @@ const cacheManager = require('./jsb-cache-manager');
                 const loopCount = Math.floor(trackEntryNative.trackTime / duration);
                 this._trackCompeleteListener && this._trackCompeleteListener(trackEntryNative, loopCount);
             } else {
-                console.log("Start time must bigger than end time!");
+                cc.warn("Start time must bigger than end time!");
             }
         });
     };
