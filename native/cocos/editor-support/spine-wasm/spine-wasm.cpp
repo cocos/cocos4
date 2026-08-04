b30a06f20b89e723a9846a93372e11dda653bde1
zxx43 <zxx43@hotmail.com>
2026-08-04 15:00:08 +0800

    sync with spine 3.8

--- native/cocos/editor-support/spine-wasm/spine-wasm.cpp
+++ native/cocos/editor-support/spine-wasm/spine-wasm.cpp
@@ -218,7 +218,7 @@ SkeletonData* SpineWasmUtil::createSpineSkeletonDataWithBinary(uint32_t byteSize
     {
         AttachmentLoader* attachmentLoader = new AtlasAttachmentLoaderExtension(atlas);
         #ifdef CC_SPINE_VERSION_3_8
-        SkeletonBinary binary(attachmentLoader);
+        SkeletonBinary binary(attachmentLoader, true);
         #else
         SkeletonBinary binary(attachmentLoader, true);
         #endif
