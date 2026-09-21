d4f18a4f1e1b8c55a08528aa39edae0dd10b2970
zxx43 <zxx43@hotmail.com>
2026-09-21 11:38:41 +0800

    fix eslint

--- cocos/particle/particle-system.ts
+++ cocos/particle/particle-system.ts
@@ -141,6 +141,7 @@ export class ParticleSystem extends ModelRenderer {
     @range([0, Number.POSITIVE_INFINITY])
     @displayOrder(10)
     @tooltip('i18n:particle_system.startSizeY')
+    // eslint-disable-next-line func-names
     @visible(function (this: ParticleSystem): boolean { return this.startSize3D; })
     public startSizeY = new CurveRange();
 
@@ -153,6 +154,7 @@ export class ParticleSystem extends ModelRenderer {
     @range([0, Number.POSITIVE_INFINITY])
     @displayOrder(10)
     @tooltip('i18n:particle_system.startSizeZ')
+    // eslint-disable-next-line func-names
     @visible(function (this: ParticleSystem): boolean { return this.startSize3D; })
     public startSizeZ = new CurveRange();
 
@@ -184,6 +186,7 @@ export class ParticleSystem extends ModelRenderer {
     @radian
     @displayOrder(12)
     @tooltip('i18n:particle_system.startRotationX')
+    // eslint-disable-next-line func-names
     @visible(function (this: ParticleSystem): boolean { return this.startRotation3D; })
     public startRotationX = new CurveRange();
 
@@ -196,6 +199,7 @@ export class ParticleSystem extends ModelRenderer {
     @radian
     @displayOrder(12)
     @tooltip('i18n:particle_system.startRotationY')
+    // eslint-disable-next-line func-names
     @visible(function (this: ParticleSystem): boolean { return this.startRotation3D; })
     public startRotationY = new CurveRange();
 
